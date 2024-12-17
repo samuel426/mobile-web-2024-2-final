b254872f0e1f38197da4e3787abbfed690bcbbe9
woo <samuel2626@khu.ac.kr>
2024-12-17 18:06:24 +0900

    add ppt and change djangoenv

--- Service_System/djangoenv2/blog/views.py
+++ Service_System/djangoenv2/blog/views.py
@@ -11,7 +11,7 @@ from rest_framework.authentication import TokenAuthentication
 # Create your views here.
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('-created_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
